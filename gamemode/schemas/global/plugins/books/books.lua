424b0700d822d9f7cf13c30c944c94f3699bfc5e
Matthew Blanchard <matthewblanc@msn.com>
2011-06-26 13:47:58 -0400

    Books are almost finished

---
+++ gamemode/schemas/global/plugins/books/books.lua
@@ -0,0 +1,6 @@
+function write( ply, handler, id, encoded, decoded )
+	book = CAKE.CreateItem("book", ply:CalcDrop(), Angle(0, 0, 0))
+	CAKE.SetUData( book:GetNWString("id"), "name", decoded["title"] )
+	CAKE.SetUData( book:GetNWString("id"), "text", decoded["text"] )
+end
+datastream.Hook( "NoteStream", write );
