fde01d6cfb998c462905d93b21b02231eb2dbb9e
Matthew Blanchard <matthewblanc@msn.com>
2011-06-30 12:18:15 -0400

    Fixed players being able to spray. Fixed items not being used correctly. Fixed notes disappearing if you copied them with no paper. Fixed being able to change the font in chat from a user end.

--- gamemode/schemas/global/plugins/chat/cl_chat.lua
+++ gamemode/schemas/global/plugins/chat/cl_chat.lua
@@ -1,4 +1,6 @@
 datastream.Hook( "TiramisuAddToChat", function( handler, id, encoded, decoded )
+    decoded.text = decoded.text:gsub("<font=%w*>", "")
+    decoded.text = decoded.text:gsub("</font>", "")
     if !decoded.font then
         decoded.font = CAKE.ChatFont
     end
@@ -20,6 +22,8 @@ datastream.Hook( "TiramisuAddToOOC", function( handler, id, encoded, decoded )
     local playername = decoded.name
     local text = decoded.text
 
+    text = text:gsub("<font=%w*>", "")
+    text = text:gsub("</font>", "")
     CAKE.Chatbox:AddLine(  "<font=" .. CAKE.ChatFont .. "><color=white>[OOC]</color><color=" .. tostring( color.r ) .. "," .. tostring( color.g ) .. "," .. tostring( color.b ) .. ">".. playername .. "</color><color=white>:" .. text .. "</color></font>", "OOC" )
 
     text = "[OOC]" .. playername .. ": " .. text
