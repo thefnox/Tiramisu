aa0dc008434559cb848c2c1f4d617dff260a11b7
Matthew Blanchard <matthewrblanchard@gmail.com>
2012-02-04 17:58:28 -0500

    Added flashlights, fixed two annoying camera bugs.

---
+++ gamemode/schemas/global/plugins/flashlight/cl_flashlight.lua
@@ -0,0 +1,28 @@
+function CAKE.FlashLightBindPress(ply, bind, pressed)
+	if string.find(bind, "impulse 100") then
+		print(bind)
+		RunConsoleCommand("rp_flashlight")
+		return true
+	end
+end
+hook.Add("PlayerBindPress", "FlashBind", CAKE.FlashLightBindPress)
+
+function CAKE.FlashLightOn(um)
+	ply = um:ReadEntity()
+	ply.flashlight = um:ReadEntity()
+	print(ply.flashlight)
+end
+usermessage.Hook( "flashlighton", CAKE.FlashLightOn )
+
+function CAKE.FlashlightThink()
+	for k,v in pairs(player.GetAll()) do
+		if v.flashlight and v.flashlight:IsValid() then
+			headpos = v:GetBonePosition(v:LookupBone("ValveBiped.Bip01_Head1"))
+			headang = ply.CurrentLookAt + v:GetAngles()
+			headnorm = headang:Forward()*14
+			v.flashlight:SetPos(headpos + headnorm)
+			v.flashlight:SetAngles((ply.CurrentLookAt + v:GetAngles()) or Angle(90,90,90))
+		end
+	end
+end
+hook.Add("PreDrawOpaqueRenderables", "FlashlightThink", CAKE.FlashlightThink)
