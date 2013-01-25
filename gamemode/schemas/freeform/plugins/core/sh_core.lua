a874c965e27845450dc09e9e067ce990643b47e7
FNox <kamaitama@gmail.com>
2013-01-25 14:18:50 -0430

    what are you doing
    Signed-off-by: FNox <kamaitama@gmail.com>

--- gamemode/schemas/freeform/plugins/core/sh_core.lua
+++
@@ -1,2 +0,0 @@
-CAKE.ConVars[ "DefaultMoney" ] = 0
-CAKE.ConVars[ "IntroSubtitle" ] = "Freeform"
--- gamemode/schemas/freeform/plugins/notes/notes.lua
+++
@@ -1,4 +0,0 @@
-concommand.Add( "rp_write", function(ply, cmd, args)
-	umsg.Start("Tiramisu.StartWrite", ply)
-	umsg.End()
-end)
