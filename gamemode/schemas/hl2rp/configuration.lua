7f23a802489fe91b5c80cb1896ba9dbfb4bd0e87
VorteX <vortex@vormangaming.nn.pe>
2013-02-12 18:18:17 +0000

    Updated some HL2RP related stuff

---
+++ gamemode/schemas/hl2rp/configuration.lua
@@ -0,0 +1,7 @@
+CAKE.CityNumber = 17 --This provides a quick way of changing the name of the city employed on the whole script
+--Since HL2RP servers seem to think that setting on a city number is a fundamental part of the community.
+
+CAKE.ConVars[ "IntroText" ] = "WELCOME TO CITY " .. CAKE.CityNumber
+CAKE.ConVars[ "IntroSubtitle" ] = "\"IT'S SAFER HERE\""
+
+CAKE.ConVars[ "MenuTitle" ] = "HALF-LIFE 2: ROLEPLAY"
--- gamemode/schemas/hl2rp/plugins/core/sh_core.lua
+++ gamemode/schemas/hl2rp/plugins/core/sh_core.lua
@@ -1,6 +1,3 @@
-CAKE.CityNumber = 17 --This provides a quick way of changing the name of the city employed on the whole script
---Since HL2RP servers seem to think that setting on a city number is a fundamental part of the community.
-
 if SERVER then	
 	--With this we disable character creation clothing.
 	CAKE.ConVars[ "DefaultClothing" ][ "Male" ] = {}
